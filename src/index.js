592dde23df0dcbad5d26b7c87539a1fdae5beda9
rtetelbaum <rtetelbaum@mailbox.org>
2020-12-06 02:10:37 -0500

    fixed renderEditUser new card render issue

--- src/index.js
+++ src/index.js
@@ -550,7 +550,7 @@ function renderEditUser() {
 					userCardsDiv.append(table)
 				} else if (card.image_url) {
 					const table = document.createElement('table')
-					table.classList.add = ("userCardsTable", "pink")
+					table.classList.add("userCardsTable", "pink")
 					table.innerHTML = `
 						<tr data-cardid = ${card.id}>
 							<td align="center">
